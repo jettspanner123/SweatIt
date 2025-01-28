2b6627e3335222835857e3042bebf0d4949709de
Uddeshya Singh <106103684+jettspanner123@users.noreply.github.com>
2025-01-29 00:13:39 +0530

    final commit

---
+++ SwiftUIScreens/Components/OptionView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionView.swift
+//  SweatIt
+//
+//  Created by Uddeshya Singh on 27/01/25.
+//
+
+import SwiftUI
+
+struct OptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionView()
+}
